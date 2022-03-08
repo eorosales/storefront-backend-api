5eaab2ef31bb4a7b8313762140eb542e505710a0
Erwin Rosales <eorosales@users.noreply.github.com>
2022-03-07 19:38:39 -0800

    Successfully set up Docker

---
+++ src/handlers/products.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import { Product, ProductStore } from '../models/product';
+
+const store = new ProductStore();
+
+const index = async (_req: Request, res: Response) => {
+  const products = await store.index();
+  res.json(products);
+}
+
+const show = async (req: Request, res: Response) => {
+  const product = await store.show(req.body.id);
+  res.json(product);
+}
+
+const create = async (req: Request, res: Response) => {
+  const product: Product = {
+    id: req.body.id,
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category
+  }
+  const newProduct = await store.create(product);
+  res.json(newProduct);
+}
+
+const product_routes = (app: express.Application) => {
+  app.get('/products', index);
+  app.get('/products/:id', show);
+  app.post('/products', create);
+}
+
+export default product_routes;
---
+++ src/models/product.ts
@@ -0,0 +1,36 @@
+import client from '../database';
+
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  category: string
+}
+
+export class ProductStore {
+  
+  async index():Promise<Product[]> {  
+    const conn = await client.connect();
+    const sql = 'SELECT * FROM products';
+    const result = await conn.query(sql);
+    conn.release();
+    return result.rows;
+  }
+  
+  async show(id:number):Promise<Product> {
+    const conn = await client.connect();
+    const sql = 'SELECT FROM products WHERE id=($1)';
+    const result = await conn.query(sql, [id]);
+    conn.release();
+    return result.rows[0];
+  }
+  
+  async create(p:Product):Promise<Product> {
+    const conn = await client.connect();
+    const sql = 'INSERT INTO products (name, price, category) VALUES ($1, $2, $3)';
+    const result = await conn.query(sql, [p.name, p.price, p. category]);
+    conn.release();
+    const product = result.rows[0];
+    return product;
+  }
+}
